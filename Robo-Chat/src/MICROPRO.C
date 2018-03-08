f44575361effbed0aedac67a22c71c257235d7ae
James Kaviyil Jose <31133182+jamesjose03@users.noreply.github.com>
2018-03-08 18:34:04 +0530

    50% Completion (Added Calendar Functionality)
    The option to display calendar implemented.

--- Robo-Chat/src/MICROPRO.C
+++ Robo-Chat/src/MICROPRO.C
@@ -5,9 +5,11 @@
 #include<windows.h>
 #define PROGRESS_BAR_SIZE 40
 #include<time.h>
+#define TRUE    1
+#define FALSE   0
 
 time_t t;
-int c1=0;
+int c1=1;
 
 
 char facts[][100]={"Banging your head against a wall burns 150 calories  an hour!","When Hippos are upset, their sweat turns red!","A flock of crows is known as a murder!","Cherophobia is the fear of fun!","King Henry VIII slept with a gigantic axe beside him.","An eagle can kill a young deer and fly away with it!","There is a species of Spider called the Hobo Spider.","A Lion's roar can be heard from 5 miles away!","Facebook, Twitter and Skype are all banned in China!" };
@@ -27,6 +29,99 @@ void display_progress_bar(int p)
     putchar(']');
 
 }
+
+
+
+int days_in_month[]={0,31,28,31,30,31,30,31,31,30,31,30,31};
+char *months[]=
+{
+	" ",
+	"\n\n\nJanuary",
+	"\n\n\nFebruary",
+	"\n\n\nMarch",
+	"\n\n\nApril",
+	"\n\n\nMay",
+	"\n\n\nJune",
+	"\n\n\nJuly",
+	"\n\n\nAugust",
+	"\n\n\nSeptember",
+	"\n\n\nOctober",
+	"\n\n\nNovember",
+	"\n\n\nDecember"
+};
+
+
+int inputyear(void)
+{
+	int year;
+
+	printf("Please enter a year (example: 1999) : ");
+	scanf("%d", &year);
+	return year;
+}
+
+int determinedaycode(int year)
+{
+	int daycode;
+	int d1, d2, d3;
+
+	d1 = (year - 1.)/ 4.0;
+	d2 = (year - 1.)/ 100.;
+	d3 = (year - 1.)/ 400.;
+	daycode = (year + d1 - d2 + d3) %7;
+	return daycode;
+}
+
+
+int determineleapyear(int year)
+{
+	if(year% 4 == FALSE && year%100 != FALSE || year%400 == FALSE)
+	{
+		days_in_month[2] = 29;
+		return TRUE;
+	}
+	else
+	{
+		days_in_month[2] = 28;
+		return FALSE;
+	}
+}
+
+void calendar(int year, int daycode)
+{
+	int month, day;
+	for ( month = 1; month <= 12; month++ )
+	{
+		printf("%s", months[month]);
+		printf("\n\nSun  Mon  Tue  Wed  Thu  Fri  Sat\n" );
+
+		// Correct the position for the first date
+		for ( day = 1; day <= 1 + daycode * 5; day++ )
+		{
+			printf(" ");
+		}
+
+		// Print all the dates for one month
+		for ( day = 1; day <= days_in_month[month]; day++ )
+		{
+			printf("%2d", day );
+
+			// Is day before Sat? Else start next line Sun.
+			if ( ( day + daycode ) % 7 > 0 )
+				printf("   " );
+			else
+				printf("\n " );
+		}
+			// Set position for next month
+			daycode = ( daycode + days_in_month[month] ) % 7;
+	}
+}
+
+
+
+
+
+
 
 int commands(char command[])
 {
@@ -104,7 +199,7 @@ else if(strcmpi(command,"rset")==0 || strcmpi(command,"set a reminder")==0 || st
         fputs("\n",fp);
         fclose(fp);
         printf("\n Successfully created reminder!");
-        c1++;
+       // c1++;
         break;
 
     //}
@@ -114,17 +209,32 @@ else if(strcmpi(command,"rset")==0 || strcmpi(command,"set a reminder")==0 || st
     else if(strcmpi(command,"Show reminders")==0 || strcmpi(command,"rshow")==0 ||strcmpi(command,"reminders")==0 || strcmpi(command,"show my reminders")==0)
 {
     int i;
-    char rem[100];
+    char c;
+    char buffer[100];
 FILE *fp = fopen("reminders.txt","r");
-for(i=1;i<=c1;i++)
+printf("\n              Title  Time   Date");
+
+while((c=getc(fp)) != EOF)
 {
-printf("\n Reminder : %s",fgets(rem,50,fp));
+
+        printf("\n Reminder %d : %c%s",c1,c,fgets(buffer,50,fp));
+        c1++;
+
 }
 fclose(fp);
 break;
 }
 
+else if(strcmpi(command,"Calendar")==0 || strcmpi(command,"Show calendar") == 0 || strcmpi(command,"1"))
+{
+    int year, daycode, leapyear;
 
+	year = inputyear();
+	daycode = determinedaycode(year);
+	determineleapyear(year);
+	calendar(year, daycode);
+	printf("\n");
+}
 
     printf("\n Sorry, I didn't understand. Please try again!");
     printf("\n Try: \n * Show me the time \n * Tell me a fact \n * Roll a die \n * Set a reminder \n * Remove a reminder \n * Show reminders");
